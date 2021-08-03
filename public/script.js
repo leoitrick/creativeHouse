c4703bb0869aca150dd540174a4f27b2ebed0034
Leandro Moreira <leoitrick@Leandros-MacBook-Pro.local>
2021-08-03 13:49:44 -0400

    fixed some css on ideas page

--- public/script.js
+++ public/script.js
@@ -1,3 +1,21 @@
 function onOff() {
   document.querySelector("#modal").classList.toggle("hide");
 }
+
+function checkFields(event) {
+  const valuesToCheck = ["title", "category", "image", "description", "link"];
+
+  const isEmpty = valuesToCheck.find(function (value) {
+    const checkIfIsString = typeof event.target[value].value === "string";
+    const checkIfIsEmpty = !event.target[value].value.trim(); //remove the spaces before and after the string
+
+    if (checkIfIsEmpty && checkIfIsString) {
+      return true;
+    }
+  });
+
+  if (isEmpty) {
+    event.preventDefault();
+    alert("Please, fill up all the fields!!!");
+  }
+}
